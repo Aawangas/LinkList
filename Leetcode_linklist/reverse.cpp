bd93cd8c12ee7e374b07b0025103265c26c24cb5
Aawangas <145684420+Aawangas@users.noreply.github.com>
2023-11-14 16:19:26 +0800

    Create reverse.cpp

---
+++ Leetcode_linklist/reverse.cpp
@@ -0,0 +1,11 @@
+ListNode* reverseList(ListNode* head) {
+    ListNode *prev = nullptr;
+    ListNode *curr = head;
+    while(curr != nullptr){
+        ListNode *next = curr ->next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+    }
+  return prev;
+}
