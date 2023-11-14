9bd00c6be9de357e96964d48ded61d9236f0d509
Aawangas <145684420+Aawangas@users.noreply.github.com>
2023-11-14 15:30:58 +0800

    Update intersection.cpp

--- Leetcode_linklist/intersection.cpp
+++ Leetcode_linklist/intersection.cpp
@@ -1,1 +1,31 @@
+ListNode *getIntersectionNode_1(ListNode *headA, ListNode *headB) {
+    /*That method uses hashtable to check if something in B occured in A*/
+    unordered_set<ListNode*> visited;
+    ListNode *current = headA;
+    for (;current != nullptr; current = current -> next){
+        visited.insert(current);
+    }
+    current = headB;
+    for(;current!=nullptr;current = current -> next){
+       if(visited.count(current)){
+           return current;
+        }
+    }
+    return nullptr;
+}
+
+ListNode *getIntersectionNode_2(ListNode *headA, ListNode *headB) {
+    //A = a+m, B = b+m, A+b = a+B, thet will reach the same point at te same time.
+    if (headA == nullptr || headB == nullptr) {
+        return nullptr;
+    }
+    ListNode *pA = headA, *pB = headB;
+    while (pA != pB) {
+        pA = pA == nullptr ? headB : pA->next;
+        pB = pB == nullptr ? headA : pB->next;
+    }
+    return pA;
+}
+
+
 
