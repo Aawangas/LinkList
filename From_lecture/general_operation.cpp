076540d20ceb5114d57635b3aed09070ed8a279b
Aawangas <145684420+Aawangas@users.noreply.github.com>
2023-11-14 14:34:52 +0800

    Update general_operation.cpp
    Adding insertion and initialization

--- From_lecture/general_operation.cpp
+++ From_lecture/general_operation.cpp
@@ -32,14 +32,34 @@ void print_linklist(list_node *head){
     }
 }
 
+void insert_node(list_node *&head, list_node *inserted,int location){
+    if (location == 0){
+        inserted -> next = head;
+        head = inserted;
+        return;
+    }
+    list_node* p = head;   
+    for(int i = 0;i<location-1 && p ->next != nullptr;i++,p = p-> next);
+    inserted -> next = p -> next;
+    p -> next = inserted;
+}
+
+list_node * initialize_node(int i){
+    /*setting next to nullptr and value to 0*/
+    list_node * node = new list_node;
+    node -> next = nullptr;
+    node -> value = i;
+    return node;
+}
+
+
 int main(){
     int array[] = {1,3,5,7,9,11,13,15};
     int size = sizeof(array)/sizeof(array[0]);
     list_node *head = create_linklist(array,size);
+    list_node * insertion = initialize_node(2);
+    insert_node(head,insertion,1);
     print_linklist(head);
-
-    
-
 
 
 
